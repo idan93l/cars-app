72df19e73e0396b517074a934682e3ff9dd50de7
Idan Lev <idan.93lev@gmail.com>
2023-02-19 23:45:12 +0200

    initialized navbar component

---
+++ client/src/app/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const NavbarContainer = styled.div`
+  min-height: 68px;
+  ${tw`
+    w-full
+    max-w-2xl
+    flex
+    flex-row
+    items-center
+    lg:pl-12
+    lg:pr-12
+    justify-between
+  `}
+`
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
