3281c7d35c2b56cc5732701f57cd4a3488002251
Idan Lev <idan.93lev@gmail.com>
2023-02-19 11:33:35 +0200

    initialized HomePage component

--- client/src/app/containers/HomePage/index.tsx
+++ client/src/app/containers/HomePage/index.tsx
@@ -1,2 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+
+const PageContainer = styled.div`
+  
+`;
+
+function HomePage() {
+  return (
+    <PageContainer>Hello from HomePage</PageContainer>
+  )
+}
+
+export default HomePage;
