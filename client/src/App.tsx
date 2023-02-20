b9b41c96cf13d5d3bc88d78c87d10e25ee7a884a
Idan Lev <idan.93lev@gmail.com>
2023-02-20 17:17:33 +0200

    few changes in app components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import Navbar from "./app/components/Navbar";
 import HomePage from "./app/containers/HomePage";
 
 const AppContainer = styled.div`
--- client/src/app/components/Logo/Logo.tsx
+++ client/src/app/components/Logo/Logo.tsx
@@ -22,7 +22,7 @@ const LogoText = styled.div`
 
 const Image = styled.div`
   width: auto;
-  ${tw`h-6 md:h-9`}
+  ${tw`flex flex-row h-6 md:h-9`}
   img {
     width: auto;
     height: 100%;
