7e47bc637a0d1041c38b9d7e1136c0e95b0978b6
Idan Lev <idan.93lev@gmail.com>
2023-02-20 16:42:18 +0200

    initialized logo component with imports

---
+++ client/src/app/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import CarLogoImg from '../../../assets/images/car-logo.png'
+import tw from 'twin.macro'
+
+
+
+function Logo() {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
