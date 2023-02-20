421e428f9883a0b528d05935306ce0b23d404ae5
Idan Lev <idan.93lev@gmail.com>
2023-02-20 16:43:09 +0200

    added logo container in logo component

--- client/src/app/components/Logo/Logo.tsx
+++ client/src/app/components/Logo/Logo.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 import CarLogoImg from '../../../assets/images/car-logo.png'
 import tw from 'twin.macro'
 
-
+const LogoContainer = styled.div`
+  ${tw`
+    flex
+    items-center
+  `}
+`
 
 function Logo() {
   return (
