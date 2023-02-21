489ef394b0d7c3b7e36f9d1b05f036bc1ac7f364
Idan Lev <idan.93lev@gmail.com>
2023-02-21 17:14:57 +0200

    added listContainer in navitems component

--- client/src/app/components/Navbar/NavItems.tsx
+++ client/src/app/components/Navbar/NavItems.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const listContainer = styled.ul`
+  ${tw`
+    flex
+    list-none
+  `}
+`
 
 function NavItems() {
   return (
