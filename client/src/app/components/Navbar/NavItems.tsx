b8152c4390d47ffdcdff3d125a82217fa70f6d75
Idan Lev <idan.93lev@gmail.com>
2023-02-21 17:21:09 +0200

    added NavItem styled component in navitems component

--- client/src/app/components/Navbar/NavItems.tsx
+++ client/src/app/components/Navbar/NavItems.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro'
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
-const listContainer = styled.ul`
+const ListContainer = styled.ul`
   ${tw`
     flex
     list-none
   `}
-`
+`;
+
+const NavItem = styled.li`
+  ${tw`
+    text-xs
+    md:text-base
+    text-black
+    font-medium
+    mr-1
+    md:mr-5
+    cursor-pointer
+    transition
+    duration-300
+    ease-in-out
+    hover:text-gray-700
+  `}
+`;
 
 function NavItems() {
-  return (
-    <div>NavItems</div>
-  )
+  return <div>NavItems</div>;
 }
 
-export default NavItems
+export default NavItems;
