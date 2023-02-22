0caa383cfea4e87a0bc72f3a51f749b6e764554e
Idan Lev <idan.93lev@gmail.com>
2023-02-22 17:51:51 +0200

    implemented NavItems in Navbar Index components jsx

--- client/src/app/components/Navbar/index.tsx
+++ client/src/app/components/Navbar/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import Logo from "../Logo/Logo";
+import NavItems from "./NavItems";
 
 const NavbarContainer = styled.div`
   min-height: 68px;
@@ -25,6 +26,7 @@ function Navbar() {
       <LogoContainer>
         <Logo />
       </LogoContainer>
+      <NavItems />
     </NavbarContainer>
   );
 }
