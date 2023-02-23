386546c320fff9ee9305c1aa16001ee8b9849a27
Idan Lev <idan.93lev@gmail.com>
2023-02-23 20:31:39 +0200

    added import usemediaquery to navitems.tsx

--- client/src/app/components/Navbar/NavItems.tsx
+++ client/src/app/components/Navbar/NavItems.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
 
 const ListContainer = styled.ul`
   ${tw`
