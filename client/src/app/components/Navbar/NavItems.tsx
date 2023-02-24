4288e363db2326f32bb4902542ddf3f75bd04548
Idan Lev <idan.93lev@gmail.com>
2023-02-24 22:07:52 +0200

    fixed error at navitems.tsx

--- client/src/app/components/Navbar/NavItems.tsx
+++ client/src/app/components/Navbar/NavItems.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import { Slide as Menu } from "react-burger-menu";
+import { slide as Menu } from "react-burger-menu";
 import { useMediaQuery } from "react-responsive";
 import { SCREENS } from "../responsive";
 import menuStyles from "./menuStyles";
