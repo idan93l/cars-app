20e9a7a2c4db55d53e60be9e39668bdee03be3df
Idan Lev <idan.93lev@gmail.com>
2023-02-25 22:28:00 +0200

    started working on topsectioncontainer at topsection.tsx

--- client/src/app/containers/HomePage/TopSection.tsx
+++ client/src/app/containers/HomePage/TopSection.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const TopSectionContainer = styled.div`
+  ${tw`
+    
+  `}
+`
 
 function TopSection() {
   return (
