7fdebebdf79785c94213c1165fd72161ebbe8c17
Idan Lev <idan.93lev@gmail.com>
2023-02-28 23:30:22 +0200

    implented TopSection at Homepage

--- client/src/app/containers/HomePage/index.tsx
+++ client/src/app/containers/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import Navbar from "../../components/Navbar";
+import TopSection from "./TopSection";
 
 const PageContainer = styled.div`
   ${tw`
@@ -18,6 +19,7 @@ function HomePage() {
   return (
     <PageContainer>
       <Navbar />
+      <TopSection />
     </PageContainer>
   );
 }
