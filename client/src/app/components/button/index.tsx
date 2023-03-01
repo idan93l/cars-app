18a33955655e9efd418dde85095808a022c47c66
Idan Lev <idan.93lev@gmail.com>
2023-03-01 22:52:36 +0200

    initialized BaseButton styled component at Button component

--- client/src/app/components/button/index.tsx
+++ client/src/app/components/button/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 interface IButtonProps {
   theme: "filled" | "outlined";
   text: string;
 }
 
-
+const BaseButton = styled.button`
+  ${tw`
+    
+  `}
+`
 
 function Button(props: IButtonProps) {
   return <div>Button</div>;
