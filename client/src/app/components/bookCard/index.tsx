b19e75e767962e7d37ae8fc0f010de02f39878b7
Idan Lev <idan.93lev@gmail.com>
2023-03-03 17:02:28 +0200

    initialized CardContainer styled component at bookCard

--- client/src/app/components/bookCard/index.tsx
+++ client/src/app/components/bookCard/index.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const CardContainer = styled.div`
+  ${tw`
+    
+  `}
+`
 
 function BookCard() {
   return (
