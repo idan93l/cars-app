96113017cebcdc44320c50b8d4a5079bad4cb323
Idan Lev <idan.93lev@gmail.com>
2023-03-03 17:41:52 +0200

    fixed Card Container at bookCard

--- client/src/app/components/bookCard/index.tsx
+++ client/src/app/components/bookCard/index.tsx
@@ -7,6 +7,8 @@ import Button from "../button";
 import { Marginer } from "../marginer";
 
 const CardContainer = styled.div`
+  min-height: 4.3em;
+  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
   ${tw`
     flex
     justify-center
@@ -19,9 +21,9 @@ const CardContainer = styled.div`
     pl-2
     md:pt-2
     md:pb-2
-    md:pl-6
-    md:pr-6
-  `}
+    md:pl-9
+    md:pr-9
+  `};
 `;
 
 const ItemContainer = styled.div`
