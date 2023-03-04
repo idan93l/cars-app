7a177ef979fb7ef1edb623c8a01eed5cac9b41f2
Idan Lev <idan.93lev@gmail.com>
2023-03-04 23:18:27 +0200

    added TopCarsContainer styled component to TopCars.tsx

---
+++ client/src/app/containers/HomePage/TopCars.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const TopCarsContainer = styled.div`
+  ${tw`
+    max-w-screen-lg
+    w-full
+    flex
+    flex-col
+    items-center
+    justify-center
+    pr-4
+    pl-4
+    md:pl-0
+    md:pr-0
+    mb-10
+  `}
+`;
+
+
+
+function TopCars() {
+  return <div>TopCars</div>;
+}
+
+export default TopCars;
