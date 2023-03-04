ad673c84d825c6bf1503f7a6b4128474cc493e86
Idan Lev <idan.93lev@gmail.com>
2023-03-04 19:31:47 +0200

    added Container styled component to BookingSteps.tsx

--- client/src/app/containers/BookingSteps.tsx
+++ client/src/app/containers/BookingSteps.tsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const Container = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-col
+    items-center
+    pt-3
+    pb-3
+    lg:pt-6
+    lg:pb-6
+  `}
+`
 
 function BookingSteps() {
   return (
