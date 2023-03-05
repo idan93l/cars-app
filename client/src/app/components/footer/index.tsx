551657e429a33458ace3f8c651938fc24cf7c534
Idan Lev <idan.93lev@gmail.com>
2023-03-05 23:38:00 +0200

    added FooterContainer styled component to footer index.tsx

--- client/src/app/components/footer/index.tsx
+++ client/src/app/components/footer/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const FooterContainer = styled.div`
+  min-height: 20em;
+  ${tw`
+    flex
+    flex-wrap
+    min-w-full
+    bg-blue-900
+    pt-7
+    pb-1
+  `}
+`
 
 function index() {
-  return (
-    <div>index</div>
-  )
+  return <div>index</div>;
 }
 
-export default index
+export default index;
