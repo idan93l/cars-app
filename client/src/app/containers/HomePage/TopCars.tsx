22558fae1cd01fed024429b79a22bb4f59147814
Idan Lev <idan.93lev@gmail.com>
2023-03-05 23:32:06 +0200

    finished for now with TopCars, should consider install @brain

--- client/src/app/containers/HomePage/TopCars.tsx
+++ client/src/app/containers/HomePage/TopCars.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { ICar } from "../../../typings/car";
 import Car from "../../components/car";
-import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel"
+import { SCREENS } from "../../components/responsive";
+import { useMediaQuery } from "react-responsive";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css/bundle';
 
 const TopCarsContainer = styled.div`
   ${tw`
@@ -39,9 +43,13 @@ const CarsContainer = styled.div`
     mt-7
     md:mt-10
   `}
-`
+`;
 
 function TopCars() {
+  // const [current, setCurrent] = useState();
+
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
   const testCar: ICar = {
     name: "Audi S3 Car",
     mileage: "10k",
@@ -68,12 +76,40 @@ function TopCars() {
     <TopCarsContainer>
       <Title>Explore Our Top Deals</Title>
       <CarsContainer>
-        <Car {...testCar}/>
-        <Car {...testCar}/>
-        <Car {...testCar2}/>
+        <Swiper
+          // style={{width: "89%", display: "flex", justifyContent: "center", alignItems: "center"}}
+          style={{width: "89%"}}
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          navigation
+          scrollbar={{ draggable: false }}
+          autoplay
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          breakpoints={{
+            640: {
+              width: 640,
+              slidesPerView: 1
+            },
+            768: {
+              width: 768,
+              slidesPerView: 2
+            },
+            900: {
+              width: 900,
+              slidesPerView: 3,
+            },
+          }}
+        >
+          <SwiperSlide><Car {...testCar}/></SwiperSlide>
+          <SwiperSlide><Car {...testCar2}/></SwiperSlide>
+          <SwiperSlide><Car {...testCar}/></SwiperSlide>
+          <SwiperSlide><Car {...testCar2}/></SwiperSlide>
+          <SwiperSlide><Car {...testCar}/></SwiperSlide>
+          <SwiperSlide><Car {...testCar2}/></SwiperSlide>
+        </Swiper>
       </CarsContainer>
     </TopCarsContainer>
-  )
+  );
 }
 
 export default TopCars;
