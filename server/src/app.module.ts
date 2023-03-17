6178ec3706d1081c200ac3fc8eff92dc8f371cbc
Idan Lev <idan.93lev@gmail.com>
2023-03-17 17:31:11 +0200

    added driver to graphqlmodule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 dotenv.config();
 
 @Module({
@@ -18,7 +19,8 @@ dotenv.config();
       entities: [],
       synchronize: true,
     }),
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       playground: (process.env.NODE_ENV === 'development' && true) || false,
       debug: (process.env.NODE_ENV === 'development' && true) || false,
     }),
@@ -26,4 +28,4 @@ dotenv.config();
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
