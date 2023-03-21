8e78c878fe0f27cad47ca8f372af7c2be491404b
Idan Lev <idan.93lev@gmail.com>
2023-03-21 15:17:56 +0200

    added forFeature to car.module.ts

--- server/src/components/cars/entities/car.ts
+++ server/src/components/cars/entities/car.ts
@@ -1,5 +1,5 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'cars' })
 @ObjectType()
