45af2d474fc1399ebf2787abd02abafa7f18796d
Idan Lev <idan.93lev@gmail.com>
2023-03-21 15:40:45 +0200

    updated cars.resolver.ts Query 1

--- server/src/components/cars/cars.resolver.ts
+++ server/src/components/cars/cars.resolver.ts
@@ -1,11 +1,12 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { CarService } from './cars.service';
+import { Car } from './entities/car';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarService) {}
 
-  @Query((returns) => String)
+  @Query((returns) => {Car})
   public async cars() {
     return 'Hello and welcome to CaRent shop!';
   }
